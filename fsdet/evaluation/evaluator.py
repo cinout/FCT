646e83fc27b7ab9972d5d2bcdc88fa6884dd7ef3
Jane Doe <haitianh@2073L-207327-M.local>
2023-02-23 17:27:37 +1100

    update

--- fsdet/evaluation/evaluator.py
+++ fsdet/evaluation/evaluator.py
@@ -199,12 +199,12 @@ def inference_on_dataset(model, data_loader, evaluator, dataset_name):
                     )
                     boxes = torch.index_select(boxes, 0, novel_predictions_idx)
 
-                    print(instances.get("image_height"))
+                    print(instances.image_height)
                     # print(instances["image_width"])
 
                     exit()
 
-                    novel_instances = detectron2.strcutures.Instances()
+                    novel_instances = detectron2.strcutures.Instances(image_size=())
 
                     instances["fields"]["pred_boxes"]["Boxes"] = boxes
                     instances["fields"]["scores"] = scores
