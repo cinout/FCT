58215b0a879dda79a9239eca358eb83b44e9de6d
Jane Doe <haitianh@2073L-207327-M.local>
2023-02-23 17:04:42 +1100

    update

--- fsdet/evaluation/evaluator.py
+++ fsdet/evaluation/evaluator.py
@@ -185,10 +185,8 @@ def inference_on_dataset(model, data_loader, evaluator, dataset_name):
                         all_classes.index(c) for c in novel_classes
                     ]
 
-                    print(instances["fields"])
+                    print(instances.get("fields"))
                     exit()
-
-
 
                     boxes = instances["fields"]["pred_boxes"]["Boxes"]  # tensor
                     scores = instances["fields"]["scores"]  # tensor
