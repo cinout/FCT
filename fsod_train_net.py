b2e44e56ff40116ced4e8ba3be823027f8f4aab3
Jane Doe <haitianh@2073L-207327-M.local>
2023-02-24 22:37:51 +1100

    update

--- fsod_train_net.py
+++ fsod_train_net.py
@@ -55,19 +55,6 @@ from collections import OrderedDict
 import detectron2.utils.comm as comm
 from detectron2.utils.logger import setup_logger
 
-# from detectron2.utils.logger import _log_api_usage
-from detectron2.utils.registry import Registry
-
-META_ARCH_REGISTRY = Registry("META_ARCH")  # noqa F401 isort:skip
-META_ARCH_REGISTRY.__doc__ = """
-Registry for meta-architectures, i.e. the whole model.
-
-The registered object will be called with `obj(cfg)`
-and expected to return a `nn.Module` object.
-"""
-print("META_ARCH_REGISTRY is:")
-print(META_ARCH_REGISTRY)
-
 
 class Trainer(DefaultTrainer):
     @classmethod
