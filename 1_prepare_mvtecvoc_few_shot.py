0c4cd2f6b014ad91979406289f080ac3edd0de6c
Jane Doe <haitianh@2073L-207327-M.local>
2023-03-24 11:12:44 +1100

    update

--- 1_prepare_mvtecvoc_few_shot.py
+++ 1_prepare_mvtecvoc_few_shot.py
@@ -146,7 +146,7 @@ def generate_seeds():
             )  # all annotation file paths by classname
 
     result = {cls: {} for cls in data_per_cat.keys()}
-    shots = [1, 2, 3, 5]  # FIXME[DONE]: change later [1, 2, 3, 5, 10]
+    shots = [1, 2, 3, 5, 15]  # FIXME[DONE]: change later [1, 2, 3, 5, 10]
 
     # we don't care seeds here, refer to prepare_pascol_xxx.py if you want to use different seeds
     random.seed(0)
